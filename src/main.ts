7cffb9252156eee24284f7a77f093682294b3922
Udbhav <udbhavs03@gmail.com>
2023-09-16 15:52:53 -0400

    run format

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from "primevue/config";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(PrimeVue);
 
-app.mount('#app')
+app.use(router);
+
+app.mount('#app');
